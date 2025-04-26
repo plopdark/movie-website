957354b7ccfc53bae04b48da175ff3504f1633ad
plopdark <ploppasha@gmail.com>
2025-04-26 13:22:49 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+})
+export class AppModule { }
